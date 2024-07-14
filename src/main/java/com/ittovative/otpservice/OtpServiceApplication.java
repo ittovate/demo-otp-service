f87a49933cc5cade7831dd416261ee6efa07a053
Mohanad <mohanadkhaled87@gmail.com>
2024-07-14 18:14:19 +0300

    Initial project template

---
+++ src/main/java/com/ittovative/otpservice/OtpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ittovative.otpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtpServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ittovative/otpservice/OtpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ittovative.otpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
