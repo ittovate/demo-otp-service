60a9d3296ff42d22b8e4b21908991252dfd72203
Mohanad <mohanadkhaled87@gmail.com>
2024-07-15 21:37:54 +0300

    Added exception handling

--- src/main/java/com/ittovative/otpservice/exception/GeneralExceptionHandler.java
+++ src/main/java/com/ittovative/otpservice/exception/GeneralExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ittovative.otpservice.exception;
 
 import com.ittovative.otpservice.util.ApiResponse;
+import org.apache.coyote.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,10 +9,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class GeneralExceptionHandler {
 
     Logger logger = LoggerFactory.getLogger(GeneralExceptionHandler.class);
+
+    @ExceptionHandler(NoSuchElementException.class)
+    ResponseEntity<ApiResponse<String>> handle(NoSuchElementException noSuchElementException){
+        logger.error(noSuchElementException.getMessage());
+        ApiResponse<String> apiResponse
+                = new ApiResponse<>(null,HttpStatus.NOT_FOUND.value(), noSuchElementException.getMessage());
+        return new ResponseEntity<>(apiResponse,HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(BadRequestException.class)
+    ResponseEntity<ApiResponse<String>> handle(BadRequestException badRequestException){
+        logger.error(badRequestException.getMessage());
+        ApiResponse<String> apiResponse
+                = new ApiResponse<>(null,HttpStatus.BAD_REQUEST.value(), badRequestException.getMessage());
+        return new ResponseEntity<>(apiResponse,HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<ApiResponse<String>> handle(Exception exception){
